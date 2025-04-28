fdb77d282886d81a446541e7eb47b840bf098b1c
Mayank Pal <mayankpal9654@gmail.com>
2025-04-28 16:40:58 +0530

    UC: Use Lombok in ContactDTO to auto-generate getters/setters and constructors

--- src/main/java/com/bridgelabz/address_book/dto/ContactDTO.java
+++ src/main/java/com/bridgelabz/address_book/dto/ContactDTO.java
@@ -1,19 +1,15 @@
 package com.bridgelabz.address_book.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//Using lombok
+@Data // Generates getters, setters, toString, equals, and hashCode
+@NoArgsConstructor
+@AllArgsConstructor
 public class ContactDTO {
     private String name;
     private String email;
-
-    public ContactDTO() {}
-
-    public ContactDTO(String name, String email) {
-        this.name = name;
-        this.email = email;
-    }
-
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
 }
